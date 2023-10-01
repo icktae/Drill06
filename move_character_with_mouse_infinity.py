230a056cd7ae144f59bffc32efdb804aef21dc65
icktae <taechil77@naver.com>
2023-10-01 23:15:32 +0900

    hand_arrow mark

--- move_character_with_mouse_infinity.py
+++ move_character_with_mouse_infinity.py
@@ -32,20 +32,29 @@ def handle_events():
 def character_image(z) :
     character.clip_draw(frame * 100, z, 100, 100, x, y)
 
-def character_move() :
-    pass
+# 거리 계산 함수
+def distance(x1, y1, x2, y2) :
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 running = True
 x = 1280 // 2
 y = 1024 // 2
 frame = 0
 
+# 클릭 위치 리스트에 저장
 click_marker = []
 click_marker_x = x
 click_marker_y = y
 
 while running :
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 
+    # hand_arrow 표시
+    for pos_x, pos_y, arrive in click_marker:
+        if not arrive:
+            hand_arrow.draw(pos_x, pos_y)
+
+        character_image(100)
 
     update_canvas()
     handle_events()
